883f51cf0f065e83280eeca18e7b0ce172b8f1dc
John Doe <webdevnorth@gmail.com>
2016-06-26 18:16:54 +0100

    request callback strips info from api response

--- src/app.js
+++ src/app.js
@@ -13,8 +13,6 @@ var db;
 var imgCollection;
 
 var googleAPI = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDVjDh8jY4rYRa8XjPiGQKkjxwVK0-ohhA&cx=018211912831661093341:svorep4zbp0&q=';
-var searchQuery = '';
-var testURL = googleAPI + 'funny giraffe'; // DELETE ME DELETE ME DELETE ME
 
 // Initialize connection once
 MongoClient.connect(mongoUrl, function(err, database) {
@@ -34,20 +32,47 @@ app.get("/", function (req, res) {
 });
 
 app.get('/:searchTerm', function (req, res) {
+  if (req.params.searchTerm) {
+    var searchTerm = req.params.searchTerm;
+    var searchURL = googleAPI + searchTerm;
+  }
+
   if (req.query.offset) {
     var offsetQuery = req.query.offset;
   }
+  
+  request(searchURL, function (err, apiRes, body) {
+    if (!err && apiRes.statusCode == 200) {
+      var bodyObj = JSON.parse(apiRes.body);      
+      var filteredResults = [];
+      
+      for (var i = 0; i < bodyObj.items.length; i++) {
 
-  if (req.params.searchTerm) {
-    var searchTerm = req.params.searchTerm;
-  }
+        if (bodyObj.items[i].link) {
+          pageLink = bodyObj.items[i].link;
+        } else {
+          var pageLink = "Missing";          
+        }
 
-  res.send('works' + offsetQuery + searchTerm);
+        if (bodyObj.items[i].title) {
+          imageTitle = bodyObj.items[i].title;
+        } else {
+          var imageTitle = "Missing";          
+        }        
+
+        // idiomatic nested property test: http://blog.osteele.com/posts/2007/12/cheap-monads/
+        var imageLinkTest = (((bodyObj.items[i].pagemap||{}).cse_image||{})[0]||{}).src;
+        if (imageLinkTest) {
+          var imageLink = bodyObj.items[i].pagemap.cse_image[0].src;
+        } else {
+          imageLink = 'Missing';
+        }
+
+        var searchResult = '{ "url": ' + pageLink + ', "description": ' + imageTitle + ', "image_url": ' + imageLink + ' }';
+        filteredResults.push(searchResult);
+      }       
+      res.send(filteredResults);  
+    }
+  });
+  
 });
-
-// request(testURL, function(err, res, body) {
-//   if (!err && res.statusCode == 200) {
-//   	console.log('THIS IS THE RESPONSE:');
-//     console.log(res.body); // Show the HTML for the queried page    
-//   }
-// });
