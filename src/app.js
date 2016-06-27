42cc797b11dc7fa2a117379eab85b067f14ab94d
John Doe <webdevnorth@gmail.com>
2016-06-27 18:23:21 +0100

    added search/date insertion to image route, reworked routing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,3 @@
-// USE CONNECTION POOLING
-
 var express = require('express');
 var app = express();
 
@@ -15,6 +13,7 @@ var imgCollection;
 var googleAPI = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDVjDh8jY4rYRa8XjPiGQKkjxwVK0-ohhA&cx=018211912831661093341:svorep4zbp0&q=';
 
 // Initialize connection once
+// Reuse database object in request handlers
 MongoClient.connect(mongoUrl, function(err, database) {
   if(err) throw err;
 
@@ -26,57 +25,79 @@ MongoClient.connect(mongoUrl, function(err, database) {
   console.log("Listening on port 3000");
 });
 
-// Reuse database object in request handlers
+// root route serves instructions
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/index.html');  
-});
+}); // end /
 
-app.get('/:searchTerm', function (req, res) {
+app.get('/img', function (req, res) {
+  // DISPLAY RECENT SEARCHES
+  // imgCollection.find({}, {"sort" : ['when', 'asc']} ).toArray(function(err,docs) {});
+  // When request is made, cull oldest searches until only the 25 latest display
+  // This avoids async issues if deletion were instead handled within the searchTerm handler
+}); // end /img
+
+// /img/searchTerm route serves API call
+app.get('/img/:searchTerm', function (req, res) {
   
   var searchTerm = req.params.searchTerm;
   var searchURL = googleAPI + searchTerm;  
   
+  // set offset of search via start query parameter
   if (req.query.offset && !isNaN(req.query.offset)) {
     console.log('Setting offsetQuery to query entry.');
     var offsetQuery = req.query.offset;
-  } else {
-    console.log('Setting offsetQuery to 0.');
-    offsetQuery = 0;       
-  }
-  searchURL = searchURL + '&start=' + offsetQuery; 
+    searchURL = searchURL + '&start=' + offsetQuery; 
+  }  
+
+  // log search term and time of search to database
+  var searchObj = {
+    term: searchTerm,
+    when: new Date()
+  };  
   
-  request(searchURL, function (err, APIRes, body) {
-    if (!err && APIRes.statusCode == 200) {
-      var bodyObj = JSON.parse(APIRes.body);      
-      var filteredResults = [];
-      
-      for (var i = 0; i < bodyObj.items.length; i++) {
-
-        if (bodyObj.items[i].link) {
-          pageLink = bodyObj.items[i].link;
-        } else {
-          var pageLink = "Missing";          
-        }
-
-        if (bodyObj.items[i].title) {
-          imageTitle = bodyObj.items[i].title;
-        } else {
-          var imageTitle = "Missing";          
-        }        
-
-        // idiomatic nested property test: http://blog.osteele.com/posts/2007/12/cheap-monads/
-        var imageLinkTest = (((bodyObj.items[i].pagemap||{}).cse_image||{})[0]||{}).src;
-        if (imageLinkTest) {
-          var imageLink = bodyObj.items[i].pagemap.cse_image[0].src;
-        } else {
-          imageLink = 'Missing';
-        }
-
-        var searchResult = '{ "url": ' + pageLink + ', "description": ' + imageTitle + ', "image_url": ' + imageLink + ' }';
-        filteredResults.push(searchResult);
-      }       
-      res.send(filteredResults);  
+  imgCollection.insertOne(searchObj, function (err, result) {
+    if (err) {
+      console.log('Insert error occurred.');
+    } else {
+      console.log('Document successfully inserted.');
     }
-  });
 
-});
+    // make http get request to the Google Custom Search API
+    request(searchURL, function (err, APIRes, body) {
+      if (!err && APIRes.statusCode == 200) {
+        console.log('Request made');
+        
+        var bodyObj = JSON.parse(APIRes.body);      
+        var filteredResults = [];
+        
+        for (var i = 0; i < bodyObj.items.length; i++) {
+
+          if (bodyObj.items[i].link) {
+            pageLink = bodyObj.items[i].link;
+          } else {
+            var pageLink = "Missing";          
+          }
+
+          if (bodyObj.items[i].title) {
+            imageTitle = bodyObj.items[i].title;
+          } else {
+            var imageTitle = "Missing";          
+          }        
+
+          // idiomatic nested property test: http://blog.osteele.com/posts/2007/12/cheap-monads/
+          var imageLinkTest = (((bodyObj.items[i].pagemap||{}).cse_image||{})[0]||{}).src;
+          if (imageLinkTest) {
+            var imageLink = bodyObj.items[i].pagemap.cse_image[0].src;
+          } else {
+            imageLink = 'Missing';
+          }
+
+          var searchResult = '{ "url": ' + pageLink + ', "description": ' + imageTitle + ', "image_url": ' + imageLink + ' }';
+          filteredResults.push(searchResult);
+        }       
+        res.send(filteredResults);  
+      }
+    }); // end request
+  }); // end imgCollection.insertOne  
+}); // end /img/:searchTerm route
